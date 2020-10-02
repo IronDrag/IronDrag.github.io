cd8c0f9c6c13f00b637cb079b90fcf09a021c6eb
Iron_Dragon <45242188+IronDrag@users.noreply.github.com>
2020-10-03 01:32:46 +0300

    Fix load timing

--- SiteScenario.js
+++ SiteScenario.js
@@ -3,7 +3,7 @@ console.log("All systems successful run!");
 
 document.addEventListener("DOMContentLoaded",function(){
  let ovrlScrn = document.querySelector(".overlay-screen");
- setTimeout(function(){ ovrlScrn.style.opacity = 0; },3000);
+ setTimeout(function(){ ovrlScrn.style.opacity = 0; },2000);
  
  setTimeout(function(){ ovrlScrn.style.display = "none"; },5000);
  
