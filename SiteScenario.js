3dcb14e5c58a383139748d27c5d9d2e8f20f214f
Iron_Dragon <45242188+IronDrag@users.noreply.github.com>
2020-10-03 01:08:35 +0300

    Add anim scenario

--- SiteScenario.js
+++ SiteScenario.js
@@ -1,1 +1,9 @@
 console.log("All systems successful run!");
+
+
+document.addEventListener("DOMContentLoaded",function(){
+ let ovrlScrn = document.querySelector(".overlay-screen");
+ ovrlScrn.style.opacity = 0;
+ setTimeout(function(){ ovrlScrn.style.display = "none"; },2000);
+ 
+});
