9ffc05f812b07719fd7d16c2640772b969539590
Iron_Dragon <45242188+IronDrag@users.noreply.github.com>
2020-10-04 21:14:03 +0300

    tost vue

--- SiteScenario.js
+++ SiteScenario.js
@@ -1,24 +1,28 @@
 console.log("All systems successful run!");
 
+var app = new Vue({
+  el: '#mySite',
+  data: {
+	gfThx: false,
+    	message: 'Hello user ;)'
+  }
+})
 
 document.addEventListener("DOMContentLoaded",function(){
 	let ovrlScrn = document.querySelector(".overlay-screen");
  
 	setTimeout(function(){ ovrlScrn.style.opacity = 0; },2000); 
- setTimeout(function(){ ovrlScrn.style.display = "none"; },5000); 
+ 	setTimeout(function(){ ovrlScrn.style.display = "none"; },5000); 
 });
 
-document.querySelector("#hidden_iframe").addEventListener("load",function(){if(submitted) {
-    /*window.location='/';*/
-    let thxForm = document.querySelector('.thx-form'); 
-    thxForm.classList.toggle('_on'); 
-    setTimeout(function(){ thxForm.classList.toggle('thx-form__on');},10000); 
+document.querySelector(".gform").addEventListener("submit",function(){	
+	app.gfThx = true;
+}});
+
+document.querySelector("#hidden_iframe").addEventListener("load",function(){if(app.gfThx) {
+    /*window.location='/';*/ 
+    setTimeout(function(){ app.gfThx = false; },10000); 
 }});
 
 
-var app = new Vue({
-  el: '#mySite',
-  data: {
-    message: 'Hello user ;)'
-  }
-})
+
