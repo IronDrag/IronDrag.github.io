9bf0347acc76bbe8e221fb2da337a447230c9ee7
Iron_Dragon <45242188+IronDrag@users.noreply.github.com>
2020-10-04 18:35:17 +0300

    upd: scenario

--- SiteScenario.js
+++ SiteScenario.js
@@ -2,9 +2,15 @@ console.log("All systems successful run!");
 
 
 document.addEventListener("DOMContentLoaded",function(){
- let ovrlScrn = document.querySelector(".overlay-screen");
- setTimeout(function(){ ovrlScrn.style.opacity = 0; },2000);
+	let ovrlScrn = document.querySelector(".overlay-screen");
  
- setTimeout(function(){ ovrlScrn.style.display = "none"; },5000);
- 
+	setTimeout(function(){ ovrlScrn.style.opacity = 0; },2000); 
+ setTimeout(function(){ ovrlScrn.style.display = "none"; },5000); 
+});
+
+document.querySelector("#hidden_iframe").onload = (if(submitted) {
+    /*window.location='/';*/
+    let thxForm = document.querySelector('.thx-form'); 
+    thxForm.classList.toggle('thx-form__on'); 
+    setTimeout(function(){ thxForm.classList.toggle('thx-form__on');},10000); 
 });
