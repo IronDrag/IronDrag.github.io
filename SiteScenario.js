732a18ec4c71537eb78623b379a24d3719c39633
Iron_Dragon <45242188+IronDrag@users.noreply.github.com>
2020-10-06 01:58:20 +0300

    fix filter

--- SiteScenario.js
+++ SiteScenario.js
@@ -5,19 +5,66 @@ var app = new Vue({
   data: {
 	gfThx: false,
 	message: 'Hello user ;)',
+	choise: { All: true, Logos: false, Models: false, Sites: false },
 	portfolio: [
-		{ title: "Logo", color: "#597372" },
-		{ title: "3D model", color: "#D39F8A" },
-		{ title: "Site", color: "#DAD1C2" }
+		{ id:1, tag: "Logos", title: "Logo", color: "#597372" },
+		{ id:2, tag: "Models", title: "3D model", color: "#D39F8A" },
+		{ id:3, tag: "Sites", title: "Site", color: "#DAD1C2" }
 	]
-  }
+  },
+  methods: {
+	clearAll: function(list){
+
+			this.choise = { All:true, Logos: false, Models: false, Sites: false };		
+			
+		},
+	filterHandler: function(item,state){
+			this.choise.All = false;
+			if(this.choise.All == false && this.choise[item] == true){
+				this.choise[item] = false;
+			} else {
+				this.choise[item] = true;
+			}			
+
+			if(state) {
+				console.log(item,"Shift");
+			}
+		}
+	}
 });
+/*
+new Vue({
+	el: '#Portfolio-main',
+	data: function () {
+	  return {
+		choise: { Logo: true, Models: true, Sites: true },
+		portfolio: [
+			{ tag: "Logo", title: "Logo", color: "#597372" },
+			{ tag: "Models", title: "3D model", color: "#D39F8A" },
+			{ tag: "Sites", title: "Site", color: "#DAD1C2" }
+		]
+	  }
+	},
+	computed: {
+		filteredChoise: function(){
+			this.choise
+		}
+	},
+	methods: {
+		clearAll: function(){
+			alert("tost");
+			this.choise = { Logo: true, Models: true, Sites: true };
+		}
+	}
+});*/
+
+
 
 document.addEventListener("DOMContentLoaded",function(){
 	let ovrlScrn = document.querySelector(".overlay-screen");
  
 	setTimeout(function(){ ovrlScrn.style.opacity = 0; },2000); 
- 	setTimeout(function(){ ovrlScrn.style.display = "none"; },5000); 
+ 	setTimeout(function(){ ovrlScrn.style.display = "none"; },3000); 
 });
 
 document.querySelector(".gform").addEventListener("submit",function(){	
