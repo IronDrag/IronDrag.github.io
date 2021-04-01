d6c8a0848fb9d55acba49e0981ee274416de457a
Iron_Dragon <45242188+IronDrag@users.noreply.github.com>
2021-04-01 07:24:34 +0300

    Migrate to Vue 3.0

--- SiteScenario.js
+++ SiteScenario.js
@@ -1,143 +1,167 @@
-console.log("All systems successful run!");
+console.log('All systems successful run!');
+const site = {
+  gfThx: false,
+  subMenu: false,
+};
 
-Vue.component("gallery",{
-	props: [],	
-	template: 	"<div>"+
-					"<slot></slot>"+
-				"</div>"	
-});
-Vue.component("gallery-filter",{
-	inject: ["filterHandler"],
-	props: ['fTag','filtchoise'],
-	template: 	"<ul class=\"Galery_ctrl\">"+
-					"<slot></slot>"+
-				"</ul>",
-});
-Vue.component("filter-item",{
-	inject: ["filterHandler"],	
-	props: ['fTag','filtchoise'],		
-	template: 	"<li :class=\"{ _active: filtchoise }\" @click.exact=\"filterHandler(fTag)\" @click.shift.exact=\"filterHandler(fTag,true)\" >"+
-					"{{ fTag }}"+
-				"</li>",
-});
+document.body.style.overflow = 'hidden';
 
-Vue.component("gallery-item",{
-	props: ['item'],
-	template: 	"<article class=\"Galery__item\" @click=\"tost\" :style=\"{ backgroundColor: item.color }\">"+
-					"<div>"+
-						"<h3>{{ item.title }}</h3>"+
-						"<ul class=\"Tag_list\">"+
-							"<li v-for=\"label in item.labels\" >{{ label }}</li>"+
-						"</ul>"+
-					"</div>"+
-				"</article>",
-	methods:{
-		tost(){}
-	}
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const tost = {
+    data() {
+      return {
+        portfolio: {
+          choise: {
+            All: true,
+            Design: false,
+            Model: false,
+            Web: false,
+          },
+          items: [],
+        },
+        gfThx: false,
+        message: 'Welcome on my site',
+        messages: (function* () {
+          yield 'Hello';
+          yield 'World';
+          yield 'And Tosters';
+          yield 'Bye bye...';
+        })(),
+      };
+    },
+    created() {
+      fetch('/assets/portfolio-web.json')
+        .then((responce) => responce.json())
+        .then((json) => {
+          this.portfolio.items = json;
+          console.dir(this.portfolio.items);
+        });
+    },
+    methods: {
+      hello(t) {
+        const gen = this.messages.next();
+        if (gen.done) {
+          t.target.classList.remove('pointer');
+          return;
+        }
+        this.message = gen.value;
+      },
+      filterCount(tost) {
+        for (const item in this.portfolio.choise) {
+          if (this.portfolio.choise[item] && item != tost) return true;
+        }
+        return false;
+      },
+      filterHandler(item, modK) {
+        if (item == 'All') {
+          this.portfolio.choise = { All: true, Design: false, Model: false, Web: false };
+          return true;
+        }
 
-var myApp = {
-	loading: true,	
-	gfThx: false,
-	message: 'Tost text',
-	messages: (function *gText(){
-		yield 'Hello user ;)';
-		yield 'Tost message';
-		return 'Good bye!';
-	})(),	
-	portfolio:{		
-		choise: {
-			All: true,
-			Design: false,
-			Model: false,
-			Web: false
-		},
-		items:[
-			{ id:1, tag: "Design", title: "Logo", color: "#597372", labels: [ "Design", "Vector", "Logo" ] },
-			{ id:2, tag: "Model", title: "Train", color: "#D39F8A", labels: [ "Model", "Low-poly", "Train" ] },
-			{ id:3, tag: "Web", title: "Site", color: "#DAD1C2", labels: [ "Web", "Codding", "Site" ] },
-			{ id:4, tag: "Web", title: "Web-dev", color: "#193441", labels: [ "Web", "CRM", "App" ] },
-			{ id:5, tag: "Design", title: "Design", color: "#FF7F66", labels: [ "Design", "Web", "Site" ] },
-			{ id:6, tag: "Model", title: "Car", color: "#91AA9D", labels: [ "Model", "Muscle", "Car" ] }			
-		]
-	} 
+        this.portfolio.choise.All = false;
+
+        if (!modK) {
+          let tost = { All: false, Design: false, Model: false, Web: false };
+          tost[item] = true;
+          this.portfolio.choise = tost;
+          return true;
+        } else {
+          if (
+            this.portfolio.choise.All == false &&
+            this.portfolio.choise[item] == true &&
+            this.filterCount(item)
+          ) {
+            this.portfolio.choise[item] = false;
+            return true;
+          } else {
+            this.portfolio.choise[item] = true;
+            return true;
+          }
+        }
+      },
+      submitForm(event) {
+        setTimeout(() => {
+          event.target.reset();
+        }, 1000);
+
+        this.gfThx = true;
+        gtag('event', 'contact');
+      },
+      loadForm() {
+        if (this.gfThx) {
+          //window.location='/';
+          setTimeout(() => {
+            this.gfThx = false;
+          }, 10000);
+        }
+      },
+    },
+    watch: {
+      choise(tost) {
+        for (const item in this.portfolio.choise) {
+          if (this.portfolio.choise[item] && item != tost) return true;
+        }
+        return false;
+      },
+    },
+    provide: function () {
+      return {
+        filterHandler: this.filterHandler,
+      };
+    },
   };
-var app = new Vue({
-  el: '#mySite',
-  data: myApp,
-  methods: {
-	hello(t){
-		this.message = this.messages.next().value;
-		t.target.classList.remove("pointer");
-	},
-	filterCount(tost){			
-		for (const item in this.portfolio.choise) {						
-		  if(this.portfolio.choise[item] && item != tost) return true;		  
-		}		
-		return false;
-	},	
-	filterHandler(item,modK){
-			if(item == "All"){
-				this.portfolio.choise = { All:true, Design: false, Model: false, Web: false };
-				return true;												
-			}
 
-			this.portfolio.choise.All = false;
+  const app = Vue.createApp(tost);
 
-			if(!modK) {				
-				let tost = { All:false, Design: false, Model: false, Web: false };
-				tost[item] = true;
-				this.portfolio.choise = tost;		
-				return true;
-			} else {
-				if(this.portfolio.choise.All == false && this.portfolio.choise[item] == true && this.filterCount(item)){								
-					this.portfolio.choise[item] = false;
-					return true;						
-				} else {					
-					this.portfolio.choise[item] = true;
-					return true;
-				}	
-			}
-		}
-	},
-	watch: {
-		choise(tost){
-			for (const item in this.portfolio.choise) {						
-				if(this.portfolio.choise[item] && item != tost) return true;		  
-			  }		
-			  return false;
-		}
-	},
-	provide: function(){
-		return {
-			filterHandler: this.filterHandler
-		};
-	}
+  app.component('gallery', {
+    props: [],
+    template: '<div>' + '<slot></slot>' + '</div>',
+  });
+
+  app.component('gallery-filter', {
+    inject: ['filterHandler'],
+    props: ['fTag', 'filtchoise'],
+    template: '<ul class="Galery_ctrl">' + '<slot></slot>' + '</ul>',
+  });
+
+  app.component('filter-item', {
+    inject: ['filterHandler'],
+    props: ['fTag', 'filtchoise'],
+    template:
+      '<li :class="{ _active: filtchoise }" @click.exact="filterHandler(fTag)" @click.shift.exact="filterHandler(fTag,true)" >' +
+      '{{ fTag }}' +
+      '</li>',
+  });
+
+  app.component('gallery-item', {
+    props: ['item'],
+    template:
+      '<article class="Galery__item" @click="tost" :style="{ backgroundColor: item.color }">' +
+      '<div>' +
+      '<h3>{{ item.title }}</h3>' +
+      '<ul class="Tag_list">' +
+      '<li v-for="label in item.labels" >{{ label }}</li>' +
+      '</ul>' +
+      '</div>' +
+      '</article>',
+    methods: {
+      tost() {},
+    },
+  });
+
+  app.mount('#mySite');
+
+  let ovrlScrn = document.querySelector('.overlay-screen');
+  setTimeout(function () {
+    ovrlScrn.style.opacity = 0;
+  }, 2000);
+  setTimeout(function () {
+    ovrlScrn.style.display = 'none';
+    document.body.style.overflowY = 'auto';
+  }, 3000);
+
+  document.querySelector('.sub-nav').addEventListener('click', function () {
+    site.subMenu = !site.subMenu;
+    this.classList.toggle('_active', site.subMenu);
+  });
 });
-
-document.body.style.overflow = "hidden";
-
-document.addEventListener("DOMContentLoaded",function(){
-	let ovrlScrn = document.querySelector(".overlay-screen");
-	setTimeout(function(){ ovrlScrn.style.opacity = 0; },2000); 
-	 setTimeout(function(){ 
-		 ovrlScrn.style.display = "none"; 
-		 document.body.style.overflowY = "auto";
-	},3000);   	 
-});
-
-document.querySelector(".gform").addEventListener("submit",function(){	
-	setTimeout(function(){
-		console.dir(this);
-		this.reset();}.bind(this),1000);
-	gtag('event', 'contact');		
-	app.gfThx = true;
-});
-
-document.querySelector("#hidden_iframe").addEventListener("load",function(){if(app.gfThx) {
-    /*window.location='/';*/ 
-	setTimeout(function(){ app.gfThx = false; },10000);
-}});
-
-
-
